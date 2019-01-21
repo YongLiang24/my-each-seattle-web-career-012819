be0f9b98ae8d0a5df8a6e512c35670ae685e6020
yong liang <yongliang24@gmail.com>
2019-01-21 19:16:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each (array)# put argument(s) here
   # code here
+  new_array=[]
+  num=array.length
+  while num>0
+    new_array << array[num]
+    num+=1
+  end
+  return new_array
 end
