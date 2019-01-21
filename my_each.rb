d21aaf5980878040eeb7fad51269548bf2f73c06
yong liang <yongliang24@gmail.com>
2019-01-21 19:22:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,9 +3,9 @@ def my_each (array)# put argument(s) here
   new_array=[]
   num=array.length
   count =0
-  while num>0
-    new_array << array[count]
-    num-=1
+  while count < array.length
+  #  new_array << array[count]
+    yield(arr[count])
     count+=1
   end
   return new_array
